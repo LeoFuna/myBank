1f72241f560b24df599af56ef620fcdb0027a402
leofuna <leo.funa@gmail.com>
2022-12-23 13:53:07 -0300

    feat: some methods on base repository

--- src/repository/base/baseRepository.js
+++ src/repository/base/baseRepository.js
@@ -1,9 +1,15 @@
 const { join } = require('path');
-const { readFile } = require("fs/promises");
+const { readFile, writeFile } = require("fs/promises");
+const faker = require('faker');
+
 class BaseRepository {
   constructor({ repository }) {
     this._repository = repository;
     this._databasePath = join(__dirname, '../../../', 'database');;
+  }
+
+  async _writeFileByPath(file) {
+    return await writeFile(join(this._databasePath, `${this._repository}.json`), JSON.stringify(file));
   }
 
   async find(itemId) {
@@ -14,6 +20,37 @@ class BaseRepository {
   async getAll() {
     const itemsFromDb = JSON.parse(await readFile(join(this._databasePath, `${this._repository}.json`), 'utf-8'));
     return itemsFromDb;
+  }
+
+  async update(itemId, payload) {
+    const items = await this.getAll();
+    let newItemData = {};
+    const updatedItems = items.map((item) => {
+      if (item.id === itemId) {
+        newItemData = {
+          ...item,
+          ...payload,
+        };
+        return newItemData;
+      }
+      return item;
+    })
+
+    await writeFile(join(this._databasePath, `${this._repository}.json`), JSON.stringify(updatedItems));
+
+    return newItemData;
+  }
+
+  async create(payload) {
+    const items = await this.getAll();
+    items.push({
+      id: faker.datatype.uuid(),
+      ...payload
+    });
+
+    await this._writeFileByPath(items);
+
+    return payload;
   }
 };
 
