5ad7ac173d2db6e7559e8cc6c1cce450d97d9a20
leofuna <leo.funa@gmail.com>
2022-12-23 12:57:05 -0300

    fix: removing fakeDb utils

--- src/utils/fakeDb.utils.js
+++
@@ -1,9 +0,0 @@
-const simulateQuery = (whenResolved) => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(whenResolved)
-    }, 1000)
-  })
-}
-
-module.exports = { simulateQuery };
