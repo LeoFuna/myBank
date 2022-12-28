b43502ab1cc35df1f81bdfbd84286e6235ceec74
leofuna <leo.funa@gmail.com>
2022-12-28 18:02:38 -0300

    fix: removing account service dep on transaction service and add methods for get transactions by account per period

--- src/services/transactionService.js
+++ src/services/transactionService.js
@@ -1,19 +1,59 @@
 const BaseRepository = require("../repository/base/baseRepository");
 
 class TransactionService {
-  constructor({ accountService }) {
+  constructor() {
     this.transactionRepository = new BaseRepository({ repository: 'transactions' });
-    this.accountService = accountService;
   }
 
-  async createNewTransaction({accountCode, valueInCents, type}) {
-    const { id } = await this.accountService.getAccountByCode(accountCode);
+  async _getTransactionsOrderedByDate() {
+    const transactions = await this.transactionRepository.getAll();
+    const sortedTransactions = transactions.sort((tran1, tran2) => {
+      if (new Date(tran1.createdAt).getTime() > new Date(tran2.createdAt).getTime()) return -1;
+      if (new Date(tran1.createdAt).getTime() < new Date(tran2.createdAt).getTime()) return 1;
+      return 0;
+    });
+    return sortedTransactions;
+  }
+
+  _filterTransactionsByAccountAndPeriod({transactions, accountIdFilter, initialDate, endDate}) {
+    return transactions.filter(({ accountId, createdAt }) => {
+      const isFromAccountSelected = accountId === accountIdFilter;
+      if (!initialDate || !endDate) return isFromAccountSelected;
+
+      const isInPeriod = new Date(createdAt).getTime() >= new Date(initialDate).getTime() &&
+        new Date(createdAt).getTime() <= new Date(endDate).getTime();
+      return isFromAccountSelected && isInPeriod;
+    });
+  }
+
+  async createNewTransaction({accountId, valueInCents, type}) {
     return await this.transactionRepository.create({
       valueInCents,
-      accountId: id,
+      accountId,
       type,
       createdAt: new Date(),
     })
+  }
+
+  async getTransactionsByAccountPerPeriod({ accountId, initialDate, endDate }) {
+    const transactions = await this._getTransactionsOrderedByDate();
+
+    const transactionsFromAccountByPeriod = this._filterTransactionsByAccountAndPeriod({
+      transactions,
+      accountIdFilter: accountId,
+      endDate,
+      initialDate,
+    });
+
+    const serializedTransactions = transactionsFromAccountByPeriod.reduce((acc, curr) => {
+      const onDateCreatedAt = new Date(curr.createdAt);
+      const formatedDate = new Intl.DateTimeFormat('pt-BR').format(onDateCreatedAt);
+      const currencyFormated = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(curr.valueInCents / 100);
+
+      return `${acc}\n${formatedDate} ${curr.type === 'credit' ? '+' : '-'} ${ currencyFormated }`;
+    }, '');
+
+    return serializedTransactions
   }
 }
 
